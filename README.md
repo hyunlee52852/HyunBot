582306ba74787cf89bb0810b829b268acd49392a
Hyun Lee <hyunlee52852@gmail.com>
2021-03-29 21:44:39 +0900

    DATETYPE를 STRING으로 변경하는 코드 작성

--- README.md
+++ README.md
@@ -6,10 +6,12 @@ password.txt 파일에 비밀번호랑 다 들어있음. 만들어 쓰자.
 pillow (pip install pillow)
 numpy (pip install numpy)
 pymysql (pip install pymysql)
+daytime (pip install daytime)
 
 
 TODO:
 SQL에 연결 후 내용 받아오기 - DONE
 
 받아온 내용을 이미지로 전환 - TODO
+
 카카오톡 봇과 연동 - TODO
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from PIL import Image
 import numpy as np
 import pymysql
+import datetime
 
+#password.txt 파일에서 id와 pw를 가져온다
 with open('password.txt') as f:
     logindata = f.read().splitlines() 
 id = logindata[0]
 pw = logindata[1]
 
-
+#내 서버에 있는 DB에 연결
 _db = pymysql.connect(
     user=id, 
     passwd=pw, 
@@ -15,11 +17,21 @@ _db = pymysql.connect(
     db='schedule', 
     charset='utf8'
 )
-
+#커서 지정
 cursor = _db.cursor(pymysql.cursors.DictCursor)
 
+#SQL 구문 모음
 _readtest = 'SELECT * FROM test'
-cursor.execute(_readtest)
+_select_sorted_data = 'SELECT date, period, description FROM test ORDER BY date ASC, period ASC;'
+
+#------------
+cursor.execute(_select_sorted_data)
 result = cursor.fetchall()
-print(result)
+
+for dic in result:
+    for key, value in dic.items():
+
+        if isinstance(value, datetime.date):
+            value = value.strftime("%m/%d")
+        print(key + " : " + str(value))
 
