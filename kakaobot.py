aeaca129e2cda7fd42be30491d4b5dd3e3ac41b6
root <root@hyun.us-west1-b.c.stdin-sql.internal>
2021-04-12 13:44:14 +0000

    made update picture on web server

---
+++ kakaobot.py
@@ -0,0 +1,14 @@
+# coding = utf-8
+ 
+from flask import Flask, render_template 
+ 
+app = Flask(__name__)
+ 
+@app.route("/")
+def home():
+    return render_template('img.html')
+ 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=9900,debug=True)
+
+
