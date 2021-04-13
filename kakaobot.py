7fd65fbc3e87943314e4581f8f66f8b5c431acda
root <root@hyun.us-west1-b.c.stdin-sql.internal>
2021-04-13 13:51:39 +0000

    made image code

--- kakaobot.py
+++ kakaobot.py
@@ -1,10 +1,49 @@
 # coding = utf-8
  
-from flask import Flask, render_template 
+from flask import Flask, render_template, jsonify, request
+import sys
  
 app = Flask(__name__)
- 
-@app.route("/")
+
+@app.route('/Check_Schedule', methods=['POST'])
+def schedule():
+        
+    content = request.get_json()
+    content = content['userRequest']
+    content = content['utterance']
+    
+    if content == u"안녕":
+        dataSend = {
+                {
+    "version": "2.0",
+    "template": {
+        "outputs": [
+            {
+                "simpleImage": {
+                    "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg",
+                    "altText": "보물상자입니다"
+                }
+            }
+        ]
+    }
+}
+                }
+    else :
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText":{
+                            "text" : "아직 공부하고있습니다."
+                        }
+                    }
+                ]
+            }
+        }
+    return jsonify(dataSend)
+
+@app.route('/')
 def home():
     return render_template('img.html')
  
