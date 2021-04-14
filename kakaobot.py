176b5999a87c95dde6785410e277b77035cbdf8d
Hyun Lee <hyunlee52852@gmail.com>
2021-04-14 19:27:12 +0900

    fixing bug

--- kakaobot.py
+++ kakaobot.py
@@ -12,33 +12,32 @@ def schedule():
     content = content['userRequest']
     content = content['utterance']
     
+    dataSend = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText":{
+                        "text" : "아직 공부하고있습니다."
+                    }
+                }
+            ]
+        }
+    }
     if content == u"안녕":
         dataSend = {
-                {
-    "version": "2.0",
-    "template": {
-        "outputs": [
-            {
-                "simpleImage": {
-                    "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg",
-                    "altText": "보물상자입니다"
-                }
-            }
-        ]
-    }
-}
-                }
-    else :
-        dataSend = {
-            "version": "2.0",
-            "template": {
-                "outputs": [
                     {
-                        "simpleText":{
-                            "text" : "아직 공부하고있습니다."
+                "version": "2.0",
+                "template": {
+                    "outputs": [
+                        {
+                            "simpleImage": {
+                                "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg",
+                                "altText": "보물상자입니다"
+                            }
                         }
-                    }
-                ]
+                    ]
+                }
             }
         }
     return jsonify(dataSend)
