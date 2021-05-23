4a0935fd55694adc63ded5e1bcf0f877c6b80769
hyun <hyunlee52852@gmail.com>
2021-05-23 17:59:56 +0900

    fix

--- CreateImage.py
+++ CreateImage.py
@@ -344,9 +344,9 @@ def printfooddata(cury):
         
     
 def setbottompart(cury):
-    lower_part = Image.open('Image_Files\\Down_bar.png', 'r')
+    lower_part = Image.open('Image_Files/Down_bar.png', 'r')
     lower_part = Image.composite(lower_part, Image.new('RGB', lower_part.size, 'white'), lower_part)
-    lower_part_flipped = Image.open('Image_Files\\Down_bar_Flipped.png', 'r')
+    lower_part_flipped = Image.open('Image_Files/Down_bar_Flipped.png', 'r')
     lower_part_flipped = Image.composite(lower_part_flipped, Image.new('RGB', lower_part_flipped.size, 'white'), lower_part_flipped)
     datefont = getfont(___d2coding_font, 150)
     datefontsmall = getfont(___d2coding_font, 50)
